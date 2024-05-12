ecef0715a5ee4a65d663b1851d77c0fba83a8104
Akash123412 <127007257+Akash123412@users.noreply.github.com>
2024-05-12 23:27:44 +0530

    main file for chord dht

---
+++ ChordDHT/maint.py
@@ -0,0 +1,222 @@
+import hashlib
+import threading
+import time
+import random
+
+class ChordClient:
+    def __init__(self, port_number=5000):
+        self.port_number = port_number
+        self.node_id = hashlib.sha1(str((self.port_number)).encode()).hexdigest()[:40]
+        self.ip_address = "127.0.0.1"   
+        self.finger_table = [None] * 161  # initialise finger table with None
+        self.successor_list = []
+        self.predecessor = None
+
+        # multithreading for stabilization routines
+        self.stabilization_thread = threading.Thread(target=self.periodic_stabilization)
+        self.stabilization_thread.daemon = True
+        self.stabilization_thread.start()
+        pass
+
+    def periodic_stabilization(self):
+        while True:
+            self.stabilize()
+            self.fix_fingers()
+            self.check_predecessor()
+            time.sleep(0.1)  # timely invoke stabilization routines every 100 ms
+    
+    def port(self, port_number=None):
+        if port_number is not None:
+            #settting the port number 
+            self.port_number = port_number
+            self.node_id = hashlib.sha1(str((self.port_number)).encode()).hexdigest()[:40]
+        else:
+            # printing the current poirt num
+            print(f" Current port number : {self.port_number}")
+        pass
+
+    def create(self):
+        self.finger_table[1] = self
+        self.successor_list.append(self)
+
+        pass
+    
+    def update_finger_table(self, ip , port):
+        # calculating node id for the new node
+
+        new_node_id = hashlib.sha1(str(port).encode()).hexdigest()[:40]
+
+        # finding the node on first finger which is greater than new node
+
+        for i in range (1, len(self.finger_table)):
+            finger_start = (self.node_id + 2** (i-1) % (2**160))
+
+            if finger_start > new_node_id:
+                self.finger_table[i] = (ip,port)
+                break
+            
+        if all(entry is None for entry in self.finger_table[1:]):
+            self.finger_table[1] = (ip,port)
+
+        pass
+
+    def send_request(ip, port, request_type, data):
+    # Simulate sending a request to join the DHT
+        print(f"Sending {request_type} request to {ip}:{port} with data: {data}")
+
+ 
+        response = random.choice(["success", "failure"])
+        return response
+       
+    def join(self, ip, port):
+        # Send a request to join the DHT
+        # fucntion 'to join request' to main dht node
+        response = send_request(ip,port,"join request",self.node_id)
+
+        if response == "success":
+            self.update_finger_table(ip,port)
+            self.update_succcessor_list(ip,port)
+            print("joined the dht successfully")
+        else:
+            print("Failed to join the dht")
+        pass
+
+    def put(self, key, value):
+        # Put the key-value pair in the DHT
+        self.data[key] = value
+        print(f"Key '{key}' with value '{value}' stored in the DHT.") 
+        pass
+
+    def printstate(self):
+        # Print the local state information
+        print("Local state information:")
+        print(f"Node ID: {self.node_id}")
+        print(f"IP address: {self.ip_address}")
+        print(f"Port number: {self.port_number}")
+        print("Finger table:")
+        for i, finger_entry in enumerate(self.finger_table):
+            if finger_entry:
+                print(f"  Finger {i}: {finger_entry}")
+            else:
+                print(f"  Finger {i}: None")
+        print("Successor list:")
+        for i, successor in enumerate(self.successor_list):
+            print(f"  Successor {i+1}: {successor}")
+        print(f"Predecessor: {self.predecessor}")
+        pass
+
+    def closest_preceding_node(self, key):
+        # find the closest preceding node
+        for i in range(len(self.finger_table) - 1, 0, -1):
+            if self.finger_table[i] and self.finger_table[i].node_id < key:
+                return self.finger_table[i]
+        return None
+
+
+    def find_successor(self, key):
+        # fiund the successor of the given key
+        # If the key's hash falls between the current node and its successor, return the successor
+        if self.predecessor and self.node_id < key <= self.successor_list[0].node_id:
+            return self.successor_list[0]
+
+        # else, forward the request to the closest preceding node
+        else:
+            closest_preceding_node = self.closest_preceding_node(key)
+            if closest_preceding_node:
+                # Forward request recursively
+                return closest_preceding_node.find_successor(key)
+            else:
+                # If no closest preceding node found, return the current node's successor
+                return self.successor_list[0]
+
+    def update_predecessor(self, predecessor):
+        # simply update the predevessor 
+        self.predecessor = predecessor
+
+    def update_successor(self, successor):
+        # Assuming successor is a tuple (ip_address, port_number)
+        self.successor_list[0] = successor
+
+    def update_finger_tables_on_leave(self):
+        
+        # Iterate through the finger table entries of other nodes and update them if necessary
+
+        for node in self.successor_list:
+            # the finger table of each node is stored as a list of tuples (ip_address, port_number)
+
+            for i in range(1, len(node.finger_table)):
+                # If the successor of the node is the leaving node, update it with the next successor
+                if node.finger_table[i] == (self.ip_address, self.port_number):
+                    node.finger_table[i] = self.successor_list[0]
+
+    def leave(self):
+        # Leave the network
+
+        # transfer data
+        for key, value in self.data.items():
+            successor = self.find_successor(key)
+            successor.put(key, value)
+
+        # inform successor/s about departture
+        for successor in self.successor_list:
+            successor.update_predecessor(self.predecessor)
+
+        # inform predecessor about departtrure
+        if self.predecessor:
+            self.predecessor.update_successor(self.successor_list[0])
+
+        
+        # remove node from successor list of predecessor
+        if self.predecessor:
+            self.predecessor.successor_list.remove((self.ip_address, self.port_number)) 
+
+
+        # update other nodes finger tables if necessary
+        self.update_finger_tables_on_leave()
+        pass
+
+    def stabilize(self):
+        # Get the successor's predecessor
+        successor_predecessor = self.successor_list[0].predecessor
+        
+        # if the successor's predecessor is between this node and its successor, update this node's succesor 
+        if self.node_id < successor_predecessor.node_id < self.successor_list[0].node_id:
+            self.successor_list[0] = successor_predecessor
+        
+        # Notify the new successor 
+        self.successor_list[0].notify(self)
+
+    def notify(self, predecessor_node):
+        # Update the predecessor
+        if self.predecessor is None or (predecessor_node.node_id > self.predecessor.node_id):
+            self.predecessor = predecessor_node
+
+    def fix_fingers(self):
+        # Fix fingers
+        pass
+
+    def check_predecessor(self):
+        # Check predecessor
+        pass
+
+    def handle_command(self, command):
+        # Handle commands from stdin
+        pass
+
+    def get(self, key):
+        # Get the value for the given key
+        pass
+
+
+
+
+
+
+if __name__ == "__main__":
+    chord_client = ChordClient()
+
+    while True:
+        command = input("> ")
+        if not command:
+            continue
+        chord_client.handle_command(command)
